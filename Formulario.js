54016847b51225e6f28d38e176413e58478a7b5a
Elizondoscha <jelizondo@scha.mx>
2026-04-30 15:31:49 -0600

    Delete Formulario.js

--- Formulario.js
+++
@@ -1,8 +0,0 @@
-import MesaAtencion from "./MesaAtencion";
-import Formulario from "./Formulario";
-
-export default function App() {
-  const path = window.location.pathname;
-  if (path === "/formulario") return <Formulario />;
-  return <MesaAtencion />;
-}
